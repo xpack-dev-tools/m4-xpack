efc2246201d70ef9d03d9dae42dd06f8fa1d97d7
Liviu Ionescu <ilg@livius.net>
2024-10-10 21:37:14 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -41,9 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from xPack Binary Development Tools`}
-      description="A binary package with the GNU M4 executables">
+    <Layout 
+      title={`Hello from m4`}
+      description="A binary package with the m4 executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
