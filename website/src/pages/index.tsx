317f613f34709fb26472c9c0e344c34c11ead7c0
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:33:53 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to m4`}
+      title="Welcome!"
       description="A binary package with the m4 executables">
       <HomepageHeader />
       <main>
