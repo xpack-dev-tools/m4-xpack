d4cb3b987590f457145ef2886b2e26b3df690900
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:21:58 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/m4@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/m4@1.4.19-3.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
